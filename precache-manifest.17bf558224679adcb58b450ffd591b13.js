026b5388deba18fc4d516e19be235a358ee076bf
Sebastian <31919808+Scherlock90@users.noreply.github.com>
2019-04-10 14:05:48 +0200

    Updates

---
+++ precache-manifest.17bf558224679adcb58b450ffd591b13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8799dffb2766472e6d4",
+    "url": "/cities-changer/static/js/runtime~main.e17815ac.js"
+  },
+  {
+    "revision": "9ced5fc21fdb5e782ca8",
+    "url": "/cities-changer/static/js/main.1f08980d.chunk.js"
+  },
+  {
+    "revision": "4560886a3cb028e7a3be",
+    "url": "/cities-changer/static/js/2.8a586615.chunk.js"
+  },
+  {
+    "revision": "9ced5fc21fdb5e782ca8",
+    "url": "/cities-changer/static/css/main.441c3465.chunk.css"
+  },
+  {
+    "revision": "f3d7a61cd6fe5e7273de257be6f7ca9d",
+    "url": "/cities-changer/index.html"
+  }
+];
